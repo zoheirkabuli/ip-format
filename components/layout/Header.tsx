ffabe7a63ca10d82860e8e32052bb8296ce486c1
zoheirkabuli <zoheyrk@gmail.com>
2023-04-04 12:59:08 +0330

    complete footer

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,10 +1,17 @@
-import { AppBar, Toolbar } from '@mui/material';
+import { AppBar, Toolbar, Typography } from '@mui/material';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <AppBar position="sticky">
-      <Toolbar>
-        <h1>اینترنت برای همه</h1>
+      <Toolbar component={'nav'} sx={{ justifyContent: 'center' }}>
+        <Typography
+          component={Link}
+          href={'/'}
+          sx={{ fontWeight: '1000', fontSize: '2rem' }}
+        >
+          اینترنت برای همه
+        </Typography>
       </Toolbar>
     </AppBar>
   );
